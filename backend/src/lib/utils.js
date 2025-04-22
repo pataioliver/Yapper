f6649c1ba7590b1125264df68dd2fe67be61104e
pataioliver <patai.oliver02@gmail.com>
2025-04-22 12:17:00 +0200

    proto to prod

---
+++ backend/src/lib/utils.js
@@ -0,0 +1,14 @@
+import jwt from 'jsonwebtoken';
+
+export const generateToken = (userId, res) => {
+    const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
+        expiresIn: "7d",
+    });
+
+    res.cookie('jwt', token, {
+        maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+        httpOnly: true, // cookie cannot be accessed by client side javascript
+        sameSite: 'strict', // cookie is not sent with cross-origin requests
+        secure: process.env.NODE_ENV === 'production' ? true : false,
+    });
+}
