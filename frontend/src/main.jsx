e11dc70ec44da8ba8347b1b7ab3201cc423c6015
Digi <marton@szabomarci.hu>
2025-04-23 13:27:01 +0200

    made basic pwa

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,14 +1,24 @@
-import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 
 import { BrowserRouter } from "react-router-dom";
 
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </StrictMode>
+if ("serviceWorker" in navigator) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker
+      .register("/service-worker.js")
+      .then((registration) => {
+        console.log("Service Worker registered with scope:", registration.scope);
+      })
+      .catch((error) => {
+        console.error("Service Worker registration failed:", error);
+      });
+  });
+}
+
+createRoot(document.getElementById("root")).render(  
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
 );
